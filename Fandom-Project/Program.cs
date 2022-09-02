384b2a15ed967ae42051179d146b3ee7ccc36ea8
Michael Maia <32021280+michael-maia@users.noreply.github.com>
2022-09-01 21:53:59 -0300

    Added CORS policy

--- Fandom-Project/Program.cs
+++ Fandom-Project/Program.cs
@@ -2,7 +2,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    // Mechanism that gives rights to the user to access resources from the server on a different domain (https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS)
+    options.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,6 +27,8 @@ app.UseSwagger();
 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Fandom Project API v1"));
 
 app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
