2771b86b3e36edc3bd9f8e252dbf0d7598371d9c
Michael Maia <32021280+michael-maia@users.noreply.github.com>
2022-11-21 20:42:10 -0300

    .

--- Fandom-Project/Controllers/CommunitiesController.cs
+++ Fandom-Project/Controllers/CommunitiesController.cs
@@ -14,7 +14,7 @@ using System.Collections;
 using Fandom_Project.Models.DataTransferObjects.UserModel;
 using Fandom_Project.Models.DataTransferObjects.UserCommunityModel;
 using Fandom_Project.Models.DataTransferObjects.PostModel;
-using Fandom_Project.Models.DataTransferObjects.EventModel;
+//using Fandom_Project.Models.DataTransferObjects.EventModel;
 
 namespace Fandom_Project.Controllers
 {
@@ -503,12 +503,12 @@ namespace Fandom_Project.Controllers
         /// <response code="204">There is no Posts registered to this Community</response>
         /// <response code="400">Invalid Community ID was sent from the client</response>
         /// <response code="404">A Community with this ID doesn't exist on the database</response>        
-        // GET: api/Communities/{id}/posts?slug={slug}
+        // GET: api/Communities/posts?slug={slug}
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(PostDto))]
         [ProducesResponseType(StatusCodes.Status204NoContent, Type = null)]
         [ProducesResponseType(StatusCodes.Status400BadRequest, Type = null)]
         [ProducesResponseType(StatusCodes.Status404NotFound, Type = null)]
-        [HttpGet("{id}/posts")]
+        [HttpGet("posts")]
         public IActionResult GetPostsBySlug([FromQuery] string slug)
         {
             try
@@ -765,64 +765,64 @@ namespace Fandom_Project.Controllers
         /// <response code="400">Invalid Community ID was sent from client</response>
         /// <response code="404">Community was not found</response>
         /// <response code="404">This Community don't have Events</response>
-        // GET: api/communities/{id}/events
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(EventDto))]
-        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = null)]
-        [ProducesResponseType(StatusCodes.Status404NotFound, Type = null)]
-        [HttpGet("{id}/events")]
-        public IActionResult GetEventsByCommunity(int id)
-        {
-            try
-            {
-                if (id <= 0)
-                {
-                    return StatusCode(StatusCodes.Status400BadRequest, new
-                    {
-                        message = "Invalid Community ID was sent from client"
-                    });
-                }
+        //// GET: api/communities/{id}/events
+        //[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(EventDto))]
+        //[ProducesResponseType(StatusCodes.Status400BadRequest, Type = null)]
+        //[ProducesResponseType(StatusCodes.Status404NotFound, Type = null)]
+        //[HttpGet("{id}/events")]
+        //public IActionResult GetEventsByCommunity(int id)
+        //{
+        //    try
+        //    {
+        //        if (id <= 0)
+        //        {
+        //            return StatusCode(StatusCodes.Status400BadRequest, new
+        //            {
+        //                message = "Invalid Community ID was sent from client"
+        //            });
+        //        }
 
-                if (_repository.Community.GetCommunityById(id) == null)
-                {
-                    return StatusCode(StatusCodes.Status404NotFound, new
-                    {
-                        message = "Community was not found"
-                    });
-                }
+        //        if (_repository.Community.GetCommunityById(id) == null)
+        //        {
+        //            return StatusCode(StatusCodes.Status404NotFound, new
+        //            {
+        //                message = "Community was not found"
+        //            });
+        //        }
 
-                IEnumerable<Event> events = _repository.Event.GetEventsByCommunity(id);
+        //        IEnumerable<Event> events = _repository.Event.GetEventsByCommunity(id);
 
-                if(events.Count() == 0)
-                {
-                    return StatusCode(StatusCodes.Status404NotFound, new
-                    {
-                        message = "This Community don't have Events"
-                    });
-                }
+        //        if(events.Count() == 0)
+        //        {
+        //            return StatusCode(StatusCodes.Status404NotFound, new
+        //            {
+        //                message = "This Community don't have Events"
+        //            });
+        //        }
 
-                IEnumerable<EventDto> eventsResult = _mapper.Map<IEnumerable<Event>, IEnumerable<EventDto>>(events);
+        //        IEnumerable<EventDto> eventsResult = _mapper.Map<IEnumerable<Event>, IEnumerable<EventDto>>(events);
 
-                IEnumerable<User> usersList = _repository.User.GetAllUsers(); // Saving all Users in 1 variable to avoid multiple requests to DB
+        //        IEnumerable<User> usersList = _repository.User.GetAllUsers(); // Saving all Users in 1 variable to avoid multiple requests to DB
 
-                // Adding the Author Name to the list by searching through our local User list
-                foreach(var index in eventsResult)
-                {
-                    index.AuthorName = usersList.Where(user => user.UserId.Equals(index.UserId)).Select(user => user.FullName).FirstOrDefault();
-                }
+        //        // Adding the Author Name to the list by searching through our local User list
+        //        foreach(var index in eventsResult)
+        //        {
+        //            index.AuthorName = usersList.Where(user => user.UserId.Equals(index.UserId)).Select(user => user.FullName).FirstOrDefault();
+        //        }
 
-                return StatusCode(StatusCodes.Status200OK, new
-                {
-                    body = eventsResult,
-                    message = "Successfully returned all Events created on this Community"
-                });
-            }
-            catch
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError, new
-                {
-                    message = "A error has ocurred in the service."
-                });
-            }
-        }
+        //        return StatusCode(StatusCodes.Status200OK, new
+        //        {
+        //            body = eventsResult,
+        //            message = "Successfully returned all Events created on this Community"
+        //        });
+        //    }
+        //    catch
+        //    {
+        //        return StatusCode(StatusCodes.Status500InternalServerError, new
+        //        {
+        //            message = "A error has ocurred in the service."
+        //        });
+        //    }
+        //}
     }
 }
